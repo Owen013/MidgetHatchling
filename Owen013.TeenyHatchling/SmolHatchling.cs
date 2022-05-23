42261d012dd390f57b935495903e9b7ec9cc1b5a
Owen_013 <owen@crawfordsix.com>
2022-05-23 16:29:32 -0400

    Massive cleanup

--- Owen013.TeenyHatchling/SmolHatchling.cs
+++ Owen013.TeenyHatchling/SmolHatchling.cs
@@ -1,13 +1,18 @@
-﻿using OWML.Common;
-using OWML.ModHelper;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Xml;
 using UnityEngine;
+using OWML.Common;
+using OWML.ModHelper;
 
 namespace SmolHatchling
 {
     public class SmolHatchlingAPI
     {
+        public Vector3 GetPlayerScale()
+        {
+            return SmolHatchling.playerScale;
+        }
+
         public float GetAnimSpeed()
         {
             return SmolHatchling.animSpeed;
@@ -18,65 +23,73 @@ namespace SmolHatchling
     {
         // Config vars
         float height, radius;
-        public static float animSpeed;
-        public bool enableHighPitch, enableStory;
+        public static float runSpeed, walkSpeed, acceleration, jumpPower, animSpeed;
+        public bool pitchChangeEnabled, storyEnabled, storyEnabledNow;
 
         // Mod vars
         public static SmolHatchling Instance;
-        OWScene scene;
         public static Vector3 playerScale;
         PlayerBody playerBody;
         PlayerCameraController cameraController;
         PlayerAnimController animController;
-        PlayerAudioController audioController;
-        static PlayerBreathingAudio breathingAudio;
-        CapsuleCollider playerCollider;
+        CapsuleCollider playerCollider, detectorCollider;
+        CapsuleShape detectorShape;
         ShipCockpitController cockpitController;
         ShipLogController logController;
         PlayerCloneController cloneController;
         EyeMirrorController mirrorController;
-        GameObject playerModel, playerThruster, playerMarshmallowStick;
-        AssetBundle assets;
-
-        private void Awake()
-        {
-            Instance = this;
-        }
-
-        private void Start()
-        {
-            // Load assets
-            assets = ModHelper.Assets.LoadBundle("Assets/textassets");
-            // Create patch for when character starts
-            ModHelper.HarmonyHelper.AddPostfix<PlayerCharacterController>(
-                "Start",
-                typeof(Patches),
-                nameof(Patches.CharacterStart));
-            // Create ghost grabbing patch for when player is picked up by Stranger.
-            ModHelper.HarmonyHelper.AddPostfix<GhostGrabController>(
-                "OnStartLiftPlayer",
-                typeof(Patches),
-                nameof(Patches.GhostLiftedPlayer));
-            // Create patch for when clone starts
-            ModHelper.HarmonyHelper.AddPostfix<PlayerCloneController>(
-                "Start",
-                typeof(Patches),
-                nameof(Patches.EyeCloneStart));
-            // Create patch for when mirror image starts
-            ModHelper.HarmonyHelper.AddPostfix<EyeMirrorController>(
-                "Start",
-                typeof(Patches),
-                nameof(Patches.EyeMirrorStart));
-            ModHelper.HarmonyHelper.AddPostfix<ChertDialogueSwapper>(
-                "SelectMood",
-                typeof(Patches),
-                nameof(Patches.ChertDialogueSwapped));
-            ModHelper.Console.WriteLine($"{nameof(SmolHatchling)} is ready to go!", MessageType.Success);
-        }
+        public GameObject playerModel, playerThruster, playerMarshmallowStick, npcPlayer;
+        AssetBundle textAssets;
+        List<OWAudioSource> playerAudio;
 
         public override object GetApi()
         {
             return new SmolHatchlingAPI();
+        }
+
+        void Awake()
+        {
+            Instance = this;
+        }
+
+        void Start()
+        {
+            // Load assets
+            textAssets = ModHelper.Assets.LoadBundle("Assets/textassets");
+
+            // Add patches
+            ModHelper.HarmonyHelper.AddPostfix<PlayerCharacterController>(
+                "Start", typeof(Patches), nameof(Patches.CharacterStart));
+            ModHelper.HarmonyHelper.AddPostfix<GhostGrabController>(
+                "OnStartLiftPlayer", typeof(Patches), nameof(Patches.GhostLiftedPlayer));
+            ModHelper.HarmonyHelper.AddPostfix<PlayerScreamingController>(
+                "Awake", typeof(Patches), nameof(Patches.NPCPlayerAwake));
+            ModHelper.HarmonyHelper.AddPostfix<PlayerCloneController>(
+                "Start", typeof(Patches), nameof(Patches.EyeCloneStart));
+            ModHelper.HarmonyHelper.AddPostfix<EyeMirrorController>(
+                "Start", typeof(Patches), nameof(Patches.EyeMirrorStart));
+            ModHelper.HarmonyHelper.AddPostfix<ChertDialogueSwapper>(
+                "SelectMood", typeof(Patches), nameof(Patches.ChertDialogueSwapped));
+
+            LoadManager.OnStartSceneLoad += (scene, loadScene) =>
+            {
+                switch (storyEnabled)
+                {
+                    case true:
+                        storyEnabledNow = true;
+                        break;
+                    case false:
+                        storyEnabledNow = false;
+                        break;
+                }
+            };
+
+            LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+            {
+                if (storyEnabledNow) ModHelper.Events.Unity.FireInNUpdates(() => ChangeDialogueTree(""), 60);
+            };
+
+            ModHelper.Console.WriteLine($"{nameof(SmolHatchling)} is ready to go!", MessageType.Success);
         }
 
         // Runs whenever the config is changed
@@ -85,99 +98,122 @@ namespace SmolHatchling
             base.Configure(config);
             height = config.GetSettingsValue<float>("Height (Default 1)");
             radius = config.GetSettingsValue<float>("Radius (Default 1)");
-            enableHighPitch = config.GetSettingsValue<bool>("Change Pitch Depending on Height");
-            enableStory = config.GetSettingsValue<bool>("Enable Story");
+            pitchChangeEnabled = config.GetSettingsValue<bool>("Change Pitch Depending on Height");
+            storyEnabled = config.GetSettingsValue<bool>("Enable Story (Requires Reload!)");
             Setup();
         }
 
         public void Setup()
         {
-            scene = LoadManager.s_currentScene;
-            if (scene == OWScene.SolarSystem || scene == OWScene.EyeOfTheUniverse)
+            // Make sure that the scene is the SS or Eye
+            if (WrongScene()) return;
+            // Cancel otherwise
+
+            playerScale = new Vector3(radius, height, radius);
+            animSpeed = Mathf.Pow(playerScale.z, -1);
+
+            playerBody = FindObjectOfType<PlayerBody>();
+            playerCollider = playerBody.GetComponent<CapsuleCollider>();
+            detectorCollider = playerBody.transform.Find("PlayerDetector").GetComponent<CapsuleCollider>();
+            detectorShape = playerBody.GetComponentInChildren<CapsuleShape>();
+            playerModel = playerBody.transform.Find("Traveller_HEA_Player_v2").gameObject;
+            playerThruster = playerBody.transform.Find("PlayerVFX").gameObject;
+            playerMarshmallowStick = playerBody.transform.Find("RoastingSystem").transform.Find("Stick_Root").gameObject;
+            cameraController = FindObjectOfType<PlayerCameraController>();
+            animController = FindObjectOfType<PlayerAnimController>();
+            PlayerAudioController audioController = FindObjectOfType<PlayerAudioController>();
+            PlayerBreathingAudio breathingAudio = FindObjectOfType<PlayerBreathingAudio>();
+            playerAudio = new List<OWAudioSource>()
             {
-                if (enableStory)
-                {
-                    playerScale = new Vector3(0.75f, 0.5f, 0.75f);
-                    ModHelper.Events.Unity.FireInNUpdates(() => ChangeDialogueTree(null), 60);
-                }
-                else playerScale = new Vector3(radius, height, radius);
-                animSpeed = Mathf.Pow(playerScale.z, -1);
-                playerBody = FindObjectOfType<PlayerBody>();
-                cameraController = FindObjectOfType<PlayerCameraController>();
-                animController = FindObjectOfType<PlayerAnimController>();
-                playerCollider = playerBody.GetComponent<CapsuleCollider>();
-                playerModel = playerBody.transform.Find("Traveller_HEA_Player_v2").gameObject;
-                playerThruster = playerBody.transform.Find("PlayerVFX").gameObject;
-                playerMarshmallowStick = playerBody.transform.Find("RoastingSystem").transform.Find("Stick_Root").gameObject;
-                audioController = FindObjectOfType<PlayerAudioController>();
-                breathingAudio = FindObjectOfType<PlayerBreathingAudio>();
-                if (scene == OWScene.SolarSystem)
-                {
+                audioController._oneShotSleepingAtCampfireSource,
+                audioController._oneShotSource,
+                breathingAudio._asphyxiationSource,
+                breathingAudio._breathingLowOxygenSource,
+                breathingAudio._breathingSource,
+                breathingAudio._drowningSource
+            };
+
+            switch (LoadManager.s_currentScene)
+            {
+                // If in solar system, set these ship vars
+                case OWScene.SolarSystem:
                     cockpitController = FindObjectOfType<ShipCockpitController>();
                     logController = FindObjectOfType<ShipLogController>();
-                }
-                else if (scene == OWScene.EyeOfTheUniverse)
-                {
+                    break;
+                // If at eye, set clone and mirror vars
+                case OWScene.EyeOfTheUniverse:
                     var cloneControllers = Resources.FindObjectsOfTypeAll<PlayerCloneController>();
                     cloneController = cloneControllers[0];
                     var mirrorControllers = Resources.FindObjectsOfTypeAll<EyeMirrorController>();
                     mirrorController = mirrorControllers[0];
-                }
-                ChangeSize();
+                    break;
             }
+
+            ChangeSize();
         }
 
         public void ChangeSize()
         {
-            // Resize collider
-            playerCollider.height = 2 * playerScale.y;
-            float targetRadius = (playerScale.x + playerScale.z) / 2 * 0.5f;
-            playerCollider.radius = Mathf.Min(playerCollider.height / 2, targetRadius);
-            playerCollider.center = new Vector3(0, playerScale.y - 1, 0);
-            ModHelper.Console.WriteLine($"Height: {playerCollider.height} \n Radius: {playerCollider.radius}");
-            // Change speed
-            animController._animator.speed = animSpeed;
-            // Smolify/beegify/regularify playermodel, camera, thrusters, and marshmallow stick.
+            // Required temp vars
+            float height = 2 * playerScale.y;
+            float radius = Mathf.Min(playerCollider.height / 2, (playerScale.x + playerScale.z) / 2 * 0.5f);
+            Vector3 center = new Vector3(0, playerScale.y - 1, 0);
+
+            // Change collider height and radius, and move colliders down so they touch the ground
+            playerCollider.height = detectorCollider.height = detectorShape.height = height;
+            playerCollider.radius = detectorCollider.radius = detectorShape.radius = radius;
+            playerCollider.center = detectorCollider.center = detectorShape.center = playerBody._centerOfMass
+                = playerCollider.center = detectorCollider.center = playerBody._activeRigidbody.centerOfMass
+                = center;
+
+            // Change playermodel size and animation speed
             playerModel.transform.localScale = playerScale / 10;
+            playerModel.transform.localPosition = new Vector3(0, -1.03f, -0.2f * playerScale.z);
             playerThruster.transform.localScale = playerScale;
             playerThruster.transform.localPosition = new Vector3(0, -1 + playerScale.y, 0);
-            cameraController._origLocalPosition = new Vector3(0f, -1 + 1.8496f * playerScale.y, 0.15f * playerScale.z);
-            playerMarshmallowStick.transform.localPosition =
-                new Vector3(0.25f, -1.8496f + 1.8496f * playerScale.y, 0.08f - 0.15f + 0.15f * playerScale.z);
-            // If pitch shift is enabled, then crank that pitch.
-            if (enableHighPitch)
+            animController._animator.speed = animSpeed;
+
+            // Move camera and marshmallow stick root down to match new player height
+            cameraController._origLocalPosition
+                = new Vector3(0f, -1 + 1.8496f * playerScale.y, 0.15f * playerScale.z);
+            playerMarshmallowStick.transform.localPosition
+                = new Vector3(0.25f, -1.8496f + 1.8496f * playerScale.y, 0.08f - 0.15f + 0.15f * playerScale.z);
+
+            // Change size of Ash Twin Project player clone if it exists
+            if (npcPlayer != null) npcPlayer.GetComponentInChildren<Animator>().gameObject.transform.localScale
+                    = playerScale / 10;
+
+            // Change pitch if enabled
+            switch (pitchChangeEnabled)
             {
-                float pitch = 0.5f * Mathf.Pow(playerScale.y, -1) + 0.5f;
-                audioController._oneShotSleepingAtCampfireSource.pitch = pitch;
-                audioController._oneShotSource.pitch = pitch;
-                breathingAudio._asphyxiationSource.pitch = pitch;
-                breathingAudio._breathingLowOxygenSource.pitch = pitch;
-                breathingAudio._breathingSource.pitch = pitch;
-                breathingAudio._drowningSource.pitch = pitch;
+                case true:
+                    float pitch = 0.5f * Mathf.Pow(playerScale.y, -1) + 0.5f;
+                    foreach (OWAudioSource audio in playerAudio) audio.pitch = pitch;
+                    break;
+
+                case false:
+                    foreach (OWAudioSource audio in playerAudio) audio.pitch = 1;
+                    break;
             }
-            // If pitch shift is disabled then set pitches to normal.
-            else
+
+            switch (LoadManager.s_currentScene)
             {
-                audioController._oneShotSleepingAtCampfireSource.pitch = 1;
-                audioController._oneShotSource.pitch = 1;
-                breathingAudio._asphyxiationSource.pitch = 1;
-                breathingAudio._breathingLowOxygenSource.pitch = 1;
-                breathingAudio._breathingSource.pitch = 1;
-                breathingAudio._drowningSource.pitch = 1;
-            }
-            // Offset attachments for pilot seat and ship log, but only if the scene is the Solar System!
-            if (scene == OWScene.SolarSystem)
-            {
-                cockpitController._origAttachPointLocalPos =
+                // Only do these in Solar System
+                case OWScene.SolarSystem:
+                    cockpitController._origAttachPointLocalPos =
                     new Vector3(0, 2.1849f - 1.8496f * playerScale.y, 4.2307f + 0.15f - 0.15f * playerScale.z);
-                logController._attachPoint._attachOffset = new Vector3(0f, 1.8496f - 1.8496f * playerScale.y, 0.15f - 0.15f * playerScale.z);
+                    logController._attachPoint._attachOffset =
+                        new Vector3(0f, 1.8496f - 1.8496f * playerScale.y, 0.15f - 0.15f * playerScale.z);
+                    break;
+                // Only do these at the Eye
+                case OWScene.EyeOfTheUniverse:
+                    cloneController._playerVisuals.transform.localScale = playerScale / 10;
+                    cloneController._signal._owAudioSource.pitch = playerAudio[0].pitch;
+                    mirrorController._mirrorPlayer.transform.Find("Traveller_HEA_Player_v2 (2)").localScale = playerScale / 10;
+                    break;
             }
-            else if (scene == OWScene.EyeOfTheUniverse)
-            {
-                cloneController._playerVisuals.transform.localScale = playerScale / 10;
-                cloneController._signal._owAudioSource.pitch = breathingAudio._oneShotSource.pitch;
-                mirrorController._mirrorPlayer.transform.Find("Traveller_HEA_Player_v2 (2)").localScale = playerScale / 10;
-            }
+
+            ModHelper.Console.WriteLine($"Player Scale: ({playerModel.transform.localScale * 10}),");
         }
 
         public void ChangeDialogueTree(string dialogueName)
@@ -189,8 +225,8 @@ namespace SmolHatchling
             {
                 dialogueTree = dialogueTrees[i];
                 string assetName = dialogueTree._xmlCharacterDialogueAsset.name;
-                TextAsset textAsset = assets.LoadAsset<TextAsset>(assetName);
-                if (!assets.Contains(assetName) || dialogueName != null && dialogueName != dialogueTree._characterName) continue;
+                TextAsset textAsset = textAssets.LoadAsset<TextAsset>(assetName);
+                if (!textAssets.Contains(assetName) || dialogueName != "" && dialogueName != dialogueTree._characterName) continue;
                 dialogueTree.SetTextXml(textAsset);
                 AddTranslations(textAsset.ToString());
                 dialogueTree.OnDialogueConditionsReset();
@@ -235,6 +271,12 @@ namespace SmolHatchling
                 }
             }
         }
+
+        private bool WrongScene()
+        {
+            OWScene scene = LoadManager.s_currentScene;
+            return !(scene == OWScene.SolarSystem || scene == OWScene.EyeOfTheUniverse);
+        }
     }
 
     public static class Patches
@@ -252,12 +294,17 @@ namespace SmolHatchling
                 new Vector3(0, 1.8496f - 1.8496f * playerScale.y, 0.15f - 0.15f * playerScale.z);
         }
 
+        public static void NPCPlayerAwake(PlayerScreamingController __instance)
+        {
+            SmolHatchling.Instance.npcPlayer = __instance.gameObject;
+        }
+
         public static void EyeCloneStart(PlayerCloneController __instance)
         {
             Vector3 playerScale = SmolHatchling.playerScale;
             float pitch;
             __instance._playerVisuals.transform.localScale = playerScale / 10;
-            if (SmolHatchling.Instance.enableHighPitch) pitch = 0.5f * Mathf.Pow(playerScale.y, -1f) + 0.5f;
+            if (SmolHatchling.Instance.pitchChangeEnabled) pitch = 0.5f * Mathf.Pow(playerScale.y, -1f) + 0.5f;
             else pitch = 1;
             __instance._signal._owAudioSource.pitch = pitch;
         }
@@ -268,9 +315,9 @@ namespace SmolHatchling
             __instance._mirrorPlayer.transform.Find("Traveller_HEA_Player_v2 (2)").localScale = playerScale / 10;
         }
 
-        public static void ChertDialogueSwapped(ChertDialogueSwapper __instance)
+        public static void ChertDialogueSwapped()
         {
-            if (SmolHatchling.Instance.enableStory) SmolHatchling.Instance.ChangeDialogueTree("Chert");
+            if (SmolHatchling.Instance.storyEnabledNow) SmolHatchling.Instance.ChangeDialogueTree("Chert");
         }
     }
 }
